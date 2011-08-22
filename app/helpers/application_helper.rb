8bfe14a68dce5d80014078843600a7fe8a2b993e
Adam Sawyer <asawyer@gmail.com>
2011-08-22 08:00:34 -0400

    Done with static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # Return a title on a per-page basis.
+  def title
+    base_title = "SampleApp"
+    if @title.nil?
+      base_title
+    else
+      # both of these do the same thing
+      #"#{base_title} | #{@title}"
+      base_title + " | " + @title
+    end
+  end
 end
